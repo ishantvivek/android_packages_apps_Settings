35250a6677a3e7e8aac9a8d65a420daf497d17b8
Janson Kang <temasek71@gmail.com>
2016-01-04 19:11:15 +0800

    Slim Recents: Use current available settings reset drawable

--- src/com/android/settings/temasek/RecentsPanelSettings.java
+++ src/com/android/settings/temasek/RecentsPanelSettings.java
@@ -256,7 +256,7 @@ public class RecentsPanelSettings extends SettingsPreferenceFragment implements
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         menu.add(0, MENU_RESET, 0, R.string.reset)
-                .setIcon(R.drawable.ic_action_reset)
+                .setIcon(R.drawable.ic_settings_reset)
                 .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
     }
 
